9eebeac8b136ebd2c18eee9c06d8b5bef00e6793
sora-44 <sahikast07@gmail.com>
2026-05-16 23:40:48 +0530

    main.py update

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-from src.endfield import Endfield
-from ef_cards import EFCard
-
-from ef_cards.utils.assets.core import asset_manager
+from endfield import Endfield
 import asyncio
+import json
+
 uid=4225399080
+
 async def main():
-    try:
-        ef=Endfield(debug=True)
-        ef_c=EFCard(ef=ef)
-        card=await ef_c.get_all_characters_card(uid)
-        for c in card:
-            c.show()
-    except Exception as e:
-        print(f"Error in main: {e}")
+    async with Endfield(debug=True) as ef:
+        data = await ef.get_showcase(uid)
+        with open(f"showcase_{uid}.json", "w") as f:
+            f.write(data.model_dump_json(indent=2))
+        for char in data.characters:
+            print(f"{char.name}")
+            print(f"{char.weapon}")
+            for stat in char.stats:
+                print(stat)
+            print("\n")
             
-    finally:
-        await asset_manager.close()
-        
 if __name__ == "__main__":
     asyncio.run(main())
